4b6174ec79f6b1f382725c4b8217c7412a30f489
nicom-github <mettler.nicolas@hotmail.fr>
2023-10-04 16:55:28 +0200

    Creation du projet

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,11 +9,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
 
+    /* PAGE D'ACCUEIL*/
     /**
      * * @Route("/", name="app_main_home")
      * */
     public function home():Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('main/home.html.twig');
     }
+
+    /* PAGE DE TEST*/
+    /**
+     * * @Route("/main", name="app_main_test")
+     * */
+    public function test():Response
+    {
+        return $this->render('main/test.html.twig');
+    }
+
 }
